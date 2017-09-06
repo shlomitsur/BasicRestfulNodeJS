e1ff5cf1e6c98f3370de12fe71e65f64ba61c08f
Shlomi AWS melodoProd1 <shlomi@comelody.com>
2017-09-06 10:19:01 +0000

    writing to mysql

--- server.js
+++ server.js
@@ -10,81 +10,111 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-let contacts = require('./data');
+let events = require('./data');
 
-app.get('/api/contacts', (request, response) => {
-  if (!contacts) {
-    response.status(404).json({ message: 'No contacts found.' });
-  }
-  response.json(contacts);
+var connection = mysql.createConnection({
+  host     : 'mysql-test.playbuzz.com',
+  user     : 'root',
+  password : 'Sharona12#',
+  database : 'i_want_so_much_to_hire_shlomi_tsur'
 });
 
-app.get('/api/contacts/:id', (request, response) => {
+connection.connect()
 
-  let contactId = request.params.id;
+connection.query('SELECT * from events', function (err, rows, fields) {
+  if (err) throw err
 
-  let contact = contacts.filter(contact => {
-    return contact.id == contactId;
+  console.log('The solution is: ', rows[0])
+})
+
+
+
+
+app.get('/api/events', (request, response) => {
+  if (!events) {
+    response.status(404).json({ message: 'No events found.' });
+  }
+  response.json(events);
+});
+
+app.get('/api/events/:id', (request, response) => {
+
+  let evntId = request.params.id;
+
+  let evnt = events.filter(evnt => {
+    return evnt.id == evntId;
   });
 
-  if (!contact) {
+  if (!evnt) {
     response.status(404).json({ message: 'Contact not found' });
   }
 
-  response.json(contact[0]);
+  response.json(evnt[0]);
 });
 
-app.post('/api/contacts', (request, response) => {
-
-  let contact = {
-    id: contacts.length + 1,
+app.post('/api/events', (request, response) => {
+ 
+  let evnt = {
+    id: null,
+    timestamp: request.body.timestamp,
     user_id: request.body.user_id,
     page_id: request.body.page_id,
     page_url: request.body.page_url,
-    page_referrer: request.body.page_referrer
+    page_referrer: request.body.page_referrer,
+    user_agent: request.body.user_agent,
+    screen_resolution: request.body.screen_resolution,
+    user_ip: request.body.user_ip
   };
 
-  contacts.push(contact);
+  console.log (evnt);
+  events.push(evnt);
 
-  response.json(contact);
+  connection.query('INSERT INTO events SET ? ', evnt, function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0])
+})
+
+
+  response.json(evnt);
 
 });
 
-app.put('/api/contacts/:id', (request, response) => {
+app.put('/api/events/:id', (request, response) => {
 
-  let contactId = request.params.id;
+  let evntId = request.params.id;
 
-  let contact = contacts.filter(contact => {
-    return contact.id == contactId;
+  let evnt = events.filter(evnt => {
+    return evnt.id == evntId;
   })[0];
 
-  const index = contacts.indexOf(contact);
+  const index = events.indexOf(evnt);
 
   let keys = Object.keys(request.body);
 
   keys.forEach(key => {
-    contact[key] = request.body[key];
+    evnt[key] = request.body[key];
   });
 
-  contacts[index] = contact;
+  events[index] = evnt;
 
-  // response.json({ message: `User ${contactId} updated.`});
-  response.json(contacts[index]);
+  // response.json({ message: `User ${evntId} updated.`});
+  response.json(events[index]);
 });
 
-app.delete('/api/contacts/:id', (request, response) => {
+app.delete('/api/events/:id', (request, response) => {
   
-  let contactId = request.params.id;
+  let evntId = request.params.id;
 
-  let contact = contacts.filter(contact => {
-    return contact.id == contactId;
+  let evnt = events.filter(evnt => {
+    return evnt.id == evntId;
   })[0];
 
-  const index = contacts.indexOf(contact);
+  const index = events.indexOf(evnt);
 
-  contacts.splice(index, 1);
+  events.splice(index, 1);
 
-  response.json({ message: `User ${contactId} deleted.`});
+  response.json({ message: `User ${evntId} deleted.`});
 
 });
 
@@ -95,3 +125,5 @@ const server = app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
   
 });
+
+//connection.end();
